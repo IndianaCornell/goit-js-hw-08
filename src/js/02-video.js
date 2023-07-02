21df7e19eb3df4b933214d2d62d13896d019893f
Serj Pyshkin <IndianaCornell@gmail.com>
2023-07-02 20:42:59 +0300

    second task done
    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    return localStorage.setItem(
+      'videoplayer-current-time',
+      JSON.stringify(data.seconds)
+    );
+  }, 1000)
+);
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time')))
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
